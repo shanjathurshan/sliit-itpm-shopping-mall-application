080bc4638a876ea7a47d203e76fd723f315bbaf7
shanjathurshan <jathurshanm3@gmail.com>
2024-03-30 09:47:18 +0530

    gaming room single card popup setup

--- src/pages/User/Home/HomeBanner.jsx
+++ src/pages/User/Home/HomeBanner.jsx
@@ -19,7 +19,7 @@ export default function HomeBanner() {
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
+            {/* <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
@@ -36,7 +36,7 @@ export default function HomeBanner() {
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
-            </div>
+            </div> */}
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
