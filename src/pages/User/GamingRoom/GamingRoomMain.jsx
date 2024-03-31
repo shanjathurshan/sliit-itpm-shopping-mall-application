04b29747e0df876a60f4a368410cbe0be6174f44
shanjathurshan <jathurshanm3@gmail.com>
2024-03-31 14:12:11 +0530

    Game center booking hisotry added

--- src/pages/User/GamingRoom/GamingRoomMain.jsx
+++ src/pages/User/GamingRoom/GamingRoomMain.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GamingRoomBanner from './GamingRoomBanner'
 import GamingRooms from './GamingRooms'
 import GamingRoomPopupCard from './GamingRoomPopupCard'
+import GameRoomSideBar from './GameRoomSideBar'
 
 const GamingRoomMain = () => {
   return (
